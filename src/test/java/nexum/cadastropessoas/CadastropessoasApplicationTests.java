c123dc0fa81c2eb84bdc8d961f6c189a8e3fd334
GSacata <gsacata@gmail.com>
2025-08-14 19:29:31 -0300

    First commit

---
+++ src/test/java/nexum/cadastropessoas/CadastropessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package nexum.cadastropessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastropessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
