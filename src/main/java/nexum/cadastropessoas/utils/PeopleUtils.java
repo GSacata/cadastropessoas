ada48d34ed6ce3e511012ce3f5e398646966514f
GSacata <gsacata@gmail.com>
2025-08-16 13:51:35 -0300

    Created CPF validator

--- src/main/java/nexum/cadastropessoas/utils/PeopleUtils.java
+++ src/main/java/nexum/cadastropessoas/utils/PeopleUtils.java
@@ -1,8 +1,48 @@
 package nexum.cadastropessoas.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PeopleUtils {
-    public static void CPFValidator() {
-        // TODO implement validator
+
+    public static Boolean CPFValidator(String document) {
+        String cleanDoc = PeopleUtils.onlyDigits(document);
+        if (cleanDoc.length() != 11) {
+            return false;
+        }
+        String leadingNine = cleanDoc.substring(0, 9);
+        String firstTrailing = cleanDoc.substring(9,10);
+        String secondTrailing = cleanDoc.substring(10);
+
+        String firstTVerif = CPFReturnDigit(leadingNine);
+        String secondTVerif = CPFReturnDigit(leadingNine + firstTrailing);
+
+        if (firstTrailing.equals(firstTVerif) && secondTrailing.equals(secondTVerif)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String CPFReturnDigit(String partialCleanDoc) {
+        Integer multiplier = partialCleanDoc.length() + 1;
+        Integer sum = 0;
+        String[] splitDoc = partialCleanDoc.split("");
+
+        for (String num: splitDoc) {
+            Integer numInt = Integer.parseInt(num);
+            sum += (numInt * multiplier);
+            multiplier -= 1;
+        }
+        Integer remainder = sum % 11;
+
+        if (remainder < 2) {
+            return Integer.toString(0);
+        } else {
+            return Integer.toString(11 - remainder);
+        }
     }
 
     public static void CNPJValidator() {
@@ -19,5 +59,23 @@ public class PeopleUtils {
 
     public static void EmailValidator() {
         // TODO implement validator
+    }
+
+    public static String onlyDigits(String document) {
+        String[] toArray = document.split("");
+        List<String> toDigitsList = new ArrayList<String>();
+
+        Pattern pat = Pattern.compile("\\d+");
+        
+        for (String item: toArray) {
+            Matcher mat = pat.matcher(item);
+            Boolean found = mat.find();
+            if (found) {
+                toDigitsList.add(item);
+            }
+        }
+
+        String cleanString = String.join("", toDigitsList);
+        return cleanString;
     }
 }
