b7471ffa68eeda3912476e78afa8c34061864aad
GSacata <gsacata@gmail.com>
2025-08-16 14:14:24 -0300

    Creating CNPJ validator

--- src/main/java/nexum/cadastropessoas/utils/PeopleUtils.java
+++ src/main/java/nexum/cadastropessoas/utils/PeopleUtils.java
@@ -13,7 +13,7 @@ public class PeopleUtils {
             return false;
         }
         String leadingNine = cleanDoc.substring(0, 9);
-        String firstTrailing = cleanDoc.substring(9,10);
+        String firstTrailing = cleanDoc.substring(9, 10);
         String secondTrailing = cleanDoc.substring(10);
 
         String firstTVerif = CPFReturnDigit(leadingNine);
@@ -45,8 +45,50 @@ public class PeopleUtils {
         }
     }
 
-    public static void CNPJValidator() {
-        // TODO implement validator
+    public static Boolean CNPJValidator(String document) {
+        String cleanDoc = PeopleUtils.onlyDigits(document);
+        if (cleanDoc.length() != 14) {
+            return false;
+        }
+        String leadingtwelve = cleanDoc.substring(0, 12);
+        String firstTrailing = cleanDoc.substring(12, 13);
+        String secondTrailing = cleanDoc.substring(13);
+
+        String firstTVerif = CNPJReturnDigit(leadingtwelve);
+        String secondTVerif = CNPJReturnDigit(leadingtwelve + firstTrailing);
+
+        if (firstTrailing.equals(firstTVerif) && secondTrailing.equals(secondTVerif)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String CNPJReturnDigit(String partialCleanDoc) {
+        Integer multiplier = 6;
+        Integer sum = 0;
+        String[] splitDoc = partialCleanDoc.split("");
+
+        if (partialCleanDoc.length() == 12) {
+            multiplier -= 1;
+        }
+
+        for (String num: splitDoc) {
+            Integer numInt = Integer.parseInt(num);
+            sum += (numInt * multiplier);
+            if (multiplier.equals(2)) {
+                multiplier = 9;
+            } else {
+                multiplier -= 1;
+            }
+        }
+        Integer remainder = sum % 11;
+
+        if (remainder < 2) {
+            return Integer.toString(0);
+        } else {
+            return Integer.toString(11 - remainder);
+        }
     }
 
     public static void PhoneValidator() {
