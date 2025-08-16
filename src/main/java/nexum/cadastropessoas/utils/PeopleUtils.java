7b4d5f348d56118fa5590a4de2c40c2403a1198d
GSacata <gsacata@gmail.com>
2025-08-16 10:58:11 -0300

    Created utils file

---
+++ src/main/java/nexum/cadastropessoas/utils/PeopleUtils.java
@@ -0,0 +1,23 @@
+package nexum.cadastropessoas.utils;
+
+public class PeopleUtils {
+    public static void CPFValidator() {
+        // TODO implement validator
+    }
+
+    public static void CNPJValidator() {
+        // TODO implement validator
+    }
+
+    public static void PhoneValidator() {
+        // TODO implement validator
+    }
+
+    public static void NameValidator() {
+        // TODO implement validator
+    }
+
+    public static void EmailValidator() {
+        // TODO implement validator
+    }
+}
