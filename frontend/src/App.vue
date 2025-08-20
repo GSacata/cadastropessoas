c85407641e56c193379cfe41156fd804e44e2c15
GSacata <gsacata@gmail.com>
2025-08-20 01:15:07 -0300

    Responsivity for people list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,18 +29,22 @@ body {
 #app {
   font-family: "Figtree", sans-serif;
   color: var(--color-font);
-  max-width: 1200px;
+  max-width: 1000px;
 }
 
 table thead tr td {
   background-color: var(--color-primary-darker);
   color: var(--color-font-alt);
+  overflow: hidden;
+  text-wrap: nowrap;
 }
 
 table tbody tr td {
   border-bottom: solid 1px var(--color-primary);
   border-collapse: collapse;
   background-color: var(--color-bg);
+  overflow: hidden;
+  text-wrap: nowrap;
 }
 
 .ed-del-cell {
@@ -111,5 +115,23 @@ table tbody tr td {
 .icon-container {
   width: 25px;
   height: 25px;
+}
+
+@media screen and (min-width: 993px) {
+  .c-show-on-large {
+    display: block !important;
+  }
+}
+
+@media screen and (min-width: 767px) and  (max-width: 992px) {
+  .c-show-on-medium {
+    display: block !important;
+  }
+}
+
+@media screen and (max-width: 766px) {
+  .c-show-on-small {
+    display: block !important;
+  }
 }
 </style>
--- frontend/src/components/PeopleList.vue
+++ frontend/src/components/PeopleList.vue
@@ -1,35 +1,93 @@
 <template>
-    <div container="row">
+    <div class="container mx-auto">
         <div v-if="this.people.length >= 1">
-            <table class="w-100">
-                <thead>
-                    <tr>
+
+            <!-- Small screen -->
+            <div class="c-show-on-small" style="display: none;">
+                <table class="w-100">
+                    <thead>
                         <tr class="row">
-                        <td class="col-1 text-center">Ações</td>
-                        <td class="col-5 text-start">Nome</td>
-                        <td class="col-2 text-end">Documento</td>
-                        <td class="col-2 text-end">E-Mail</td>
-                        <td class="col-2 text-end">Telefone</td>
-                    </tr>
-                    </tr>
-                </thead>
-                <tbody class="container">
-                    <tr class="row" v-for="person of people" :key="person.uuid">
-                        <td class="col-1 text-center ed-del-cell">
-                            <span @click="toRegister(person.uuid)" class="icon-container">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-                            </span>
-                            <span @click="showModal(person.uuid)" class="icon-container">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-                            </span>
-                        </td>
-                        <td class="col-5 text-start">{{ person.name }}</td>
-                        <td class="col-2 text-end">{{ person.document }}</td>
-                        <td class="col-2 text-end">{{ person.email }}</td>
-                        <td class="col-2 text-end">{{ person.phone }}</td>
-                    </tr>
-                </tbody>
-            </table>
+                            <td class="col-2 text-center">Ações</td>
+                            <td class="col-6 text-start">Nome pequena</td>
+                            <td class="col-4 text-end">Documento</td>
+                        </tr>
+                    </thead>
+                    <tbody class="container">
+                        <tr class="row" v-for="person of people" :key="person.uuid">
+                            <td class="col-2 text-center ed-del-cell">
+                                <span @click="toRegister(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+                                </span>
+                                <span @click="showModal(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                                </span>
+                            </td>
+                            <td class="col-6 text-start">{{ person.name }}</td>
+                            <td class="col-4 text-end">{{ person.document }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Medium screen -->
+            <div class="c-show-on-medium" style="display: none;">
+                <table class="w-100">
+                    <thead>
+                        <tr class="row">
+                            <td class="col-1 text-center">Ações</td>
+                            <td class="col-5 text-start">Nome média</td>
+                            <td class="col-3 text-end">Documento</td>
+                            <td class="col-3 text-end">E-Mail</td>
+                        </tr>
+                    </thead>
+                    <tbody class="container">
+                        <tr class="row" v-for="person of people" :key="person.uuid">
+                            <td class="col-1 text-center ed-del-cell">
+                                <span @click="toRegister(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+                                </span>
+                                <span @click="showModal(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                                </span>
+                            </td>
+                            <td class="col-5 text-start">{{ person.name }}</td>
+                            <td class="col-3 text-end">{{ person.document }}</td>
+                            <td class="col-3 text-end">{{ person.email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Large screen -->
+            <div class="c-show-on-large" style="display: none;">
+                <table class="w-100">
+                    <thead>
+                        <tr class="row">
+                            <td class="col-1 text-center">Ações</td>
+                            <td class="col-5 text-start">Nome grande</td>
+                            <td class="col-2 text-end">Documento</td>
+                            <td class="col-2 text-end">E-Mail</td>
+                            <td class="col-2 text-end">Telefone</td>
+                        </tr>
+                    </thead>
+                    <tbody class="container">
+                        <tr class="row" v-for="person of people" :key="person.uuid">
+                            <td class="col-1 text-center ed-del-cell">
+                                <span @click="toRegister(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
+                                </span>
+                                <span @click="showModal(person.uuid)" class="icon-container">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill=var(--color-primary)><path d="m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                                </span>
+                            </td>
+                            <td class="col-5 text-start">{{ person.name }}</td>
+                            <td class="col-2 text-end">{{ person.document }}</td>
+                            <td class="col-2 text-end">{{ person.email }}</td>
+                            <td class="col-2 text-end">{{ person.phone }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div v-else class="text-center">
             Nenhuma pessoa cadastrada.
